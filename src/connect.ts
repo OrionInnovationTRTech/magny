5cc3dd43dd698528d1292620ccb25f636948da7a
Kaaleyah <furkan.baytemur@orioninc.com>
2022-08-30 11:13:14 +0300

    Camera button is active

---
+++ src/connect.ts
@@ -0,0 +1,46 @@
+
+const servers = {
+    iceServers : [
+        {
+            urls: [ 'stun:stun.l.google.com:19302', 
+                    'stun:stun1.l.google.com:19302', 
+                    'stun:stun2.l.google.com:19302', 
+                    'stun:stun3.l.google.com:19302', 
+                    'stun:stun4.l.google.com:19302' ]
+        }
+    ],
+    iceCandidatePoolSize: 10
+}
+
+const peerConnection = new RTCPeerConnection(servers);
+
+
+export function createOffer() {
+
+}
+
+export async function startLocalCamera() {
+    let localStream = await navigator.mediaDevices.getUserMedia({ video: true, audio: true });
+
+    // Push tracks from localStream to peerConnection
+    localStream.getTracks().forEach( track => {
+        peerConnection.addTrack(track, localStream);
+    })
+
+    // Add stream to local video element
+    const localVideo = document.querySelector('#localVideo') as HTMLVideoElement;
+    localVideo.srcObject = localStream;
+}
+
+export async function getRemoteCamera() {
+    let remoteStream = new MediaStream();
+
+    peerConnection.ontrack = event => {
+        event.streams[0].getTracks().forEach( track => {
+            remoteStream.addTrack(track);
+        })
+    }
+
+    const remoteVideo = document.querySelector('#remoteVideo') as HTMLVideoElement;
+    remoteVideo.srcObject = remoteStream;
+}
--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,11 @@
 import './style.css'
+
+import { startLocalCamera, getRemoteCamera } from './connect';
+
+
+const cameraButton = document.querySelector('#webcamButton') as HTMLButtonElement;
+
+cameraButton.addEventListener('click', async () => {
+    await startLocalCamera();
+    await getRemoteCamera();
+})
