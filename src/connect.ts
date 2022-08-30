84d03acf3438a516fb1a73a4d88f59eeb9576ba4
Kaaleyah <furkan.baytemur@orioninc.com>
2022-08-30 16:44:28 +0300

    Firebase import

--- src/connect.ts
+++ src/connect.ts
@@ -1,4 +1,3 @@
-
 const servers = {
     iceServers : [
         {
@@ -13,3 +12,23 @@ const servers = {
 }
 
 export const peerConnection = new RTCPeerConnection(servers);
+
+
+// Firebase
+import { initializeApp } from 'firebase/app';
+import { getFirestore, doc, getDoc, getDocs, query, collection, addDoc, updateDoc, deleteDoc } from  'firebase/firestore'
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAXWuMBpXGtNwZ4o9EYQR74Pl_XqW-Xp7Q",
+    authDomain: "magny-1035a.firebaseapp.com",
+    projectId: "magny-1035a",
+    storageBucket: "magny-1035a.appspot.com",
+    messagingSenderId: "413650997463",
+    appId: "1:413650997463:web:71a21f809062adeb332092",
+    measurementId: "G-LT0D4P5QLT"
+};
+  
+const fireApp = initializeApp(firebaseConfig);
+const firestore = getFirestore(fireApp);
